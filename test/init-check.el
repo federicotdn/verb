b5de5c18e58c3066d7fdbd033a42fa66d5da73c6
Federico Tedin <federicotedin@gmail.com>
2024-04-27 20:22:51 +0200

    Fix CI?

--- test/init-check.el
+++ test/init-check.el
@@ -6,6 +6,11 @@
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
+
+;; Update this if CI breaks for older Emacs versions
+(when (= emacs-major-version 26)
+  (setq package-check-signature nil))
+
 (dolist (pkg '(cl-lib let-alist compat package-lint xr relint))
   (unless (package-installed-p pkg)
     (package-install pkg)))
